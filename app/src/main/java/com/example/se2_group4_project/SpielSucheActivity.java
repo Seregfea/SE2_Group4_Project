2500f09b96a1c52f4d166e20da6c46e8c11cb0bf
Yevhen Andrianov <yevhenandrianov@MacBook-Air-Yevhen.local>
2023-04-15 19:01:54 +0200

    Implement first view activity

---
+++ app/src/main/java/com/example/se2_group4_project/SpielSucheActivity.java
@@ -0,0 +1,14 @@
+package com.example.se2_group4_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpielSucheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spiel_suche);
+    }
+}
