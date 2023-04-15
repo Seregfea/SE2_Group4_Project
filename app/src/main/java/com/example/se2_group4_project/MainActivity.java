670188ac23ad6a0f9810981d736b9b8c855003a1
Yevhen Andrianov <yevhenandrianov@MacBook-Air-Yevhen.local>
2023-04-15 20:06:55 +0200

    Create PlayActivity

--- app/src/main/java/com/example/se2_group4_project/MainActivity.java
+++ app/src/main/java/com/example/se2_group4_project/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 import com.example.se2_group4_project.screens.FindGameActivity;
 import com.example.se2_group4_project.screens.OptionsActivity;
+import com.example.se2_group4_project.screens.PlayGameActivity;
 import com.example.se2_group4_project.screens.SelectRoomActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button bntPlay = findViewById(R.id.button_play);
+        Button btnPlay = findViewById(R.id.button_play);
         Button btnFindGame = findViewById(R.id.button_find_game);
         Button btnOptions = findViewById(R.id.button_options);
         Button btnSelectRoom = findViewById(R.id.button_select_room);
 
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PlayGameActivity.class);
+                startActivity(intent);
+            }
+        });
 
         btnFindGame.setOnClickListener(new View.OnClickListener() {
             @Override
