b85d94183fd7e05d25def25d95de0cffbe2ddacf
verenarath <verenarath@edu.aau.at>
2023-04-19 23:03:31 +0200

    Aendern der Dateinamen und Anpassung der JSON und versuchte Testklasse

---
+++ app/src/test/java/com/example/se2_group4_project/CardDrawerTest.java
@@ -0,0 +1,17 @@
+package com.example.se2_group4_project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class CardDrawerTest {
+
+//    @Test
+//    void generateInitialCards() {
+//        CardDrawer cardDrawer = new CardDrawer();
+//        assertEquals(cardDrawer.getPlayerBlueStack(), new ArrayList<>());
+////        cardDrawer.generateInitialCards();
+//    }
+}
