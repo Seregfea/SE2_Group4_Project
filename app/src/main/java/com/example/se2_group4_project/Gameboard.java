d955b7d3aa9b00c1fad94c692c3274557fa951cb
CarinaBaeck <cabaeck@edu.aau.at>
2023-05-01 19:50:07 +0200

    Gameboard:
    -dynamically size of layout
    -fixing some bugs

--- app/src/main/java/com/example/se2_group4_project/Gameboard.java
+++ app/src/main/java/com/example/se2_group4_project/Gameboard.java
@@ -6,10 +6,19 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.se2_group4_project.cards.Card;
+import com.example.se2_group4_project.cards.CardDrawer;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Gameboard extends AppCompatActivity {
     private DicePopUpActivity dicePopUpActivity;
     private Button btnRollDice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +34,20 @@ public class Gameboard extends AppCompatActivity {
                 dicePopUpActivity.rollDice();
             }
         });
+
+        CardDrawer cardDrawer = new CardDrawer();
+        try {
+            cardDrawer.generateInitialCards();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        LinearLayout linearLayout = findViewById(R.id.UserCardsLayout);
+
+        // ich greife auf stack zu und habe index 0 - 4
+        // ich nehme die playerBlueStack z.B. und bekomme da meine Images
+        // jeder Spieler bekommt Nummer mit einer Farbe random - von dieser Farbe bekommt er Karten
+        // Layout passt sich an
+
+
     }
 }
