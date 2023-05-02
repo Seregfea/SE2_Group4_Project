25344a6ed77611f87fda2115d7034b61d787d3c8
CarinaBaeck <cabaeck@edu.aau.at>
2023-05-02 17:55:33 +0200

    Gameboard:
    -card stacks layouts

--- app/src/main/java/com/example/se2_group4_project/screens/PlayGameActivity.java
+++ app/src/main/java/com/example/se2_group4_project/screens/PlayGameActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.se2_group4_project.Gameboard;
 import com.example.se2_group4_project.MainActivity;
 import com.example.se2_group4_project.R;
 
