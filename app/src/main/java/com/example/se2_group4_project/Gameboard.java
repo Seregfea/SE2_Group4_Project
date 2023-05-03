42357f90603179c94ea6967370f26ac154b0bc7a
Christian <chraunjak@edu.aau.at>
2023-05-03 20:43:27 +0200

    space

--- app/src/main/java/com/example/se2_group4_project/Gameboard.java
+++ app/src/main/java/com/example/se2_group4_project/Gameboard.java
@@ -16,6 +16,7 @@ import android.widget.LinearLayout;
 
 import com.example.se2_group4_project.cards.Card;
 import com.example.se2_group4_project.cards.CardDrawer;
+import com.example.se2_group4_project.gameboard_layouts.CardsLayoutLeft;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -38,6 +39,31 @@ public class Gameboard extends AppCompatActivity {
                 dicePopUpActivity.rollDice();
             }
         });
+        LinearLayout linearLayout = findViewById(R.id.UserCardsLayout);
+        CardsLayoutLeft left = new CardsLayoutLeft(linearLayout);
+
+        ImageView drei = new ImageView(this);
+        drei.setImageResource(R.drawable.bad_dreckig_hellblau);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(60, LinearLayout.LayoutParams.MATCH_PARENT);
+        drei.setLayoutParams(params);
+
+// create the second ImageView and set its width to 60dp
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.bad_dreckig_hellblau);
+        params = new LinearLayout.LayoutParams(60, LinearLayout.LayoutParams.MATCH_PARENT);
+        imageView.setLayoutParams(params);
+
+// create the third ImageView and set its width to 60dp
+        ImageView zwei = new ImageView(this);
+        zwei.setImageResource(R.drawable.couch_dreckig_orange);
+        params = new LinearLayout.LayoutParams(60, LinearLayout.LayoutParams.MATCH_PARENT);
+        zwei.setLayoutParams(params);
+
+// add the ImageViews to the layout
+        left.addImage(drei);
+        left.addImage(imageView);
+        left.addImage(zwei);
+
         CardDrawer c = new CardDrawer(this.getApplicationContext());
         try {
             c.generateInitialCards();
@@ -65,3 +91,19 @@ public class Gameboard extends AppCompatActivity {
 
     }
 }
+
+/*CardDrawer cardDrawer = new CardDrawer();
+        try {
+            cardDrawer.generateInitialCards();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        LinearLayout linearLayout = findViewById(R.id.UserCardsLayout);
+
+        // ich greife auf stack zu und habe index 0 - 4
+        // ich nehme die playerBlueStack z.B. und bekomme da meine Images
+        // jeder Spieler bekommt Nummer mit einer Farbe random - von dieser Farbe bekommt er Karten
+        // Layout passt sich an
+
+
+         */
