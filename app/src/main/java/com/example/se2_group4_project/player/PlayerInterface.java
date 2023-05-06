4349019b8e23f68eb0f9fbd87e45212787c09335
zhekaandri <36640457+zhekaandri@users.noreply.github.com>
2023-05-06 15:54:06 +0200

    Remove throwDice method from interface

--- app/src/main/java/com/example/se2_group4_project/player/PlayerInterface.java
+++ app/src/main/java/com/example/se2_group4_project/player/PlayerInterface.java
@@ -1,7 +1,6 @@
 package com.example.se2_group4_project.player;
 
 public interface PlayerInterface {
-    void throwDice();
     void takeCard();
     void flipCard();
     void takeCardFromAnotherPlayer();
