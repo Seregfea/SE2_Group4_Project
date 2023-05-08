87cbd3168086d8abb11108360f5f7fe277139f91
Max Krassnitzer <m1krassnit@edu.aau.at>
2023-05-08 21:09:15 +0200

    roommate_cards: implemented roommate logic and changes in json objects - implemented roommateEasy and roommateDiffcult logic - json object structure changed (removed neededDice property and added isFront property)

---
+++ app/src/main/java/com/example/se2_group4_project/cards/RoommateDiffType.java
@@ -0,0 +1,12 @@
+package com.example.se2_group4_project.cards;
+
+public enum RoommateDiffType {
+    BUKOWSKI,
+    FW,
+    HERTA,
+    KRAPOTKE,
+    MU,
+    MARIA,
+    OV,
+    SARAH
+}
---
+++ app/src/main/java/com/example/se2_group4_project/cards/RoommateEasy.java
@@ -0,0 +1,48 @@
+package com.example.se2_group4_project.cards;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RoommateEasy {
+    private int additionalDice = 0;
+    private int number;
+    private int count;
+    private boolean awake = false;
+
+    public RoommateEasy(JSONObject roommateEasy) throws JSONException {
+        this.number = roommateEasy.getInt("number");
+        this.count = roommateEasy.getInt("count");
+    }
+
+    public int getAdditionalDice() {
+        return additionalDice;
+    }
+
+    public void setAdditionalDice(int additionalDice) {
+        this.additionalDice = additionalDice;
+    }
+
+    public boolean isAwake() {
+        return awake;
+    }
+
+    public void setAwake(boolean awake) {
+        this.awake = awake;
+
+        if (awake) {
+            setAdditionalDice(1);
+        } else {
+            setAdditionalDice(0);
+        }
+    }
+
+    public boolean isAvailable(int[] dice) {
+        for (int i = 0; i < dice.length; i++) {
+            if (dice[i] >= count && i+1 == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
