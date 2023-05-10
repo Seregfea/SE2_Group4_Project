d44902bfc96569f9fe142eb54db70f6c834e30a5
Max Krassnitzer <m1krassnit@edu.aau.at>
2023-05-10 19:05:36 +0200

    playerLogic: implemented sortedCountDiceArray with Kangaroo

--- app/src/main/java/com/example/se2_group4_project/dices/Dice.java
+++ app/src/main/java/com/example/se2_group4_project/dices/Dice.java
@@ -35,6 +35,27 @@ public class Dice {
         return countDiceNumbers;
     }
 
+    public static int[] sortedCountDiceArrayKangaroo(List<Integer> dice) {
+        int[] countDiceNumbers = new int[6];
+
+        for (int i = 0; i < dice.size(); i++) {
+            if (dice.get(i) == 1) {
+                countDiceNumbers[0] += 1;
+            } else if (dice.get(i) == 2) {
+                countDiceNumbers[1] += 1;
+            } else if (dice.get(i) == 3) {
+                countDiceNumbers[2] += 1;
+            } else if (dice.get(i) == 4) {
+                countDiceNumbers[3] += 1;
+            } else if (dice.get(i) == 5) {
+                countDiceNumbers[4] += 1;
+            } else if (dice.get(i) == 6) {
+                countDiceNumbers[5] += 1;
+            }
+        }
+        return countDiceNumbers;
+    }
+
     public static int getTotalDiceResult(List<Integer> results) {
         int sum = 0;
 
