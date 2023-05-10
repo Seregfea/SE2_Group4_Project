450978924ec721bfb3560293dc9ff1310dc16f45
zhekaandri <36640457+zhekaandri@users.noreply.github.com>
2023-05-10 18:29:42 +0200

    Change Json for item, implement new functions for item cards

--- app/src/main/java/com/example/se2_group4_project/cards/Item.java
+++ app/src/main/java/com/example/se2_group4_project/cards/Item.java
@@ -1,23 +1,64 @@
 package com.example.se2_group4_project.cards;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Item {
     private int id;
     private String name;
     private int number;
     private int count;
+    private int stealCard;
     private String itemBenefit;
     private String cardFront;
     private boolean isFront;
 
-    public Item(int id, String name, int number, int count, String itemBenefit, String cardFront, boolean isFront) {
-        this.id = id;
-        this.name = name;
-        this.number = number;
-        this.count = count;
-        this.itemBenefit = itemBenefit;
-        this.cardFront = cardFront;
-        this.isFront = isFront;
+    private boolean couchMatschig = false;
+    private boolean badewanneDreckig = false;
+    private boolean geschirrDreckig = false;
+
+
+    public Item(JSONObject item) throws JSONException {
+        this.number = item.getInt("number");
+        this.count = item.getInt("count");
+        this.stealCard = item.getInt("stealCard");
+
+        switch (item.getString("itemBenefit")){
+            case "Couch matschig":
+                this.couchMatschig = true;
+                break;
+            case "Badewanne dreckig":
+                this.badewanneDreckig = true;
+                break;
+            case "Geschirr dreckig":
+                this.geschirrDreckig = true;
+                break;
+            default:
+                System.out.println("Keine item Benefits");
+        }
     }
+
+    // Methods
+    public boolean isAvailable(int[] rolledDice){
+        for (int i = 0; i < rolledDice.length; i++){
+            if (rolledDice[i] >= count && i+1 == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isStealable(int[] rolledDice){
+        for(int i = 0; i < rolledDice.length; i++){
+            if (rolledDice[i] >= stealCard && i+1 == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // Getters/Setters
 
     public int getId() {
         return id;
@@ -41,6 +82,14 @@ public class Item {
 
     public void setNumber(int number) {
         this.number = number;
+    }
+
+    public int getStealCard(){
+        return stealCard;
+    }
+
+    public void setStealCard(int stealCard){
+        this.stealCard = stealCard;
     }
 
     public int getCount() {
