5d18b0dc4aec744ddb6bdb9540f90e1707e93a5d
CarinaBaeck <cabaeck@edu.aau.at>
2023-05-23 17:42:45 +0200

    Gameboard Music Option:
    -added activity_music.xml
    -added MusicActivity.java
    -added buttons and seekBar for music volume options

--- app/src/main/java/com/example/se2_group4_project/screens/MusicActivity.java
+++ app/src/main/java/com/example/se2_group4_project/screens/MusicActivity.java
@@ -1,10 +1,14 @@
 package com.example.se2_group4_project.screens;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.se2_group4_project.MainActivity;
@@ -18,12 +22,55 @@ public class MusicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_music);
 
         Button btnBack = findViewById(R.id.button_back);
+        Button btnBackgrMusic = findViewById(R.id.buttonBackgroundMusic);
+        SeekBar seekBarVolume = findViewById(R.id.seekBarVolume);
+        Switch switchMute = findViewById(R.id.mute);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), OptionsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        /*btnBackgrMusic.setOnClickListener(new View.OnClickListener) {
+            @Override
+            public void onClick (View v){
+                new AlertDialog.Builder(MusicActivity.this)
+                        .setTitle("Wähle einen Song")
+                        .setItems(songTitles, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Der 'which' Parameter enthält den Index des ausgewählten Elements.
+                                String selectedSong = songTitles[which];
+
+                                // Hier können Sie die Logik zum Ändern der Musik einfügen.
+                                // Zum Beispiel könnten Sie eine Methode aufrufen, um die aktuell gespielte Musik zu stoppen und den ausgewählten Song zu starten.
+
+                            }
+                        })
+                        .show();
+            }
+        });
+
+         */
+
+        seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Hier können Sie die Lautstärke entsprechend dem Fortschritt der SeekBar einstellen.
+                float volume = progress / 100f;
+                // mediaPlayer.setVolume(volume, volume); // Sie müssen den MediaPlayer hier zugänglich machen.
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Wird aufgerufen, wenn der Benutzer beginnt, die SeekBar zu bewegen.
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Wird aufgerufen, wenn der Benutzer aufhört, die SeekBar zu bewegen.
             }
         });
     }
