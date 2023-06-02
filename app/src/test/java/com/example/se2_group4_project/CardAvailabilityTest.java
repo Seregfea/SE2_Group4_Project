81f986701a4bba0369ebe1051e72b4c3d3767960
CarinaBaeck <cabaeck@edu.aau.at>
2023-06-02 19:05:18 +0200

    Gameboard_Adjustments:
    -added new CardAvailability test class

---
+++ app/src/test/java/com/example/se2_group4_project/CardAvailabilityTest.java
@@ -0,0 +1,51 @@
+package com.example.se2_group4_project;
+
+import org.junit.jupiter.api.Test;
+
+public class CardAvailabilityTest {
+
+
+    @Test
+    public void testCheckAvailability() {
+
+        /*
+
+
+        // Create mock cards
+        Item mockItem = Mockito.mock(Item.class);
+        RoommateDifficult mockRoommateDifficult = Mockito.mock(RoommateDifficult.class);
+        RoommateEasy mockRoommateEasy = Mockito.mock(RoommateEasy.class);
+        Troublemaker mockTroublemaker = Mockito.mock(Troublemaker.class);
+        WitzigToDos mockWitzigToDos = Mockito.mock(WitzigToDos.class);
+        WitzigWitzigToDos mockWitzigWitzigToDos = Mockito.mock(WitzigWitzigToDos.class);
+
+        // Mock the isAvailable method for each card
+        when(mockItem.isAvailable(any())).thenReturn(true);
+        when(mockRoommateDifficult.isAvailable(any())).thenReturn(true);
+        when(mockRoommateEasy.isAvailable(any())).thenReturn(true);
+        when(mockTroublemaker.isAvailable(any())).thenReturn(true);
+        when(mockWitzigToDos.isAvailable(any())).thenReturn(true);
+        when(mockWitzigWitzigToDos.isAvailable(any())).thenReturn(true);
+
+        // Create a list with all mock cards
+        ArrayList<Card> allCardsOnGameboard = new ArrayList<>(Arrays.asList(
+                mockItem, mockRoommateDifficult, mockRoommateEasy, mockTroublemaker, mockWitzigToDos, mockWitzigWitzigToDos));
+
+        CardAvailability cardAvailability = new CardAvailability(allCardsOnGameboard);
+
+        int[] dice = {1, 2, 3, 4, 5, 6};
+        cardAvailability.checkAvailability(dice);
+
+        // Verify that isAvailable was called for each card
+        verify(mockItem).isAvailable(dice);
+        verify(mockRoommateDifficult).isAvailable(dice);
+        verify(mockRoommateEasy).isAvailable(dice);
+        verify(mockTroublemaker).isAvailable(dice);
+        verify(mockWitzigToDos).isAvailable(dice);
+        verify(mockWitzigWitzigToDos).isAvailable(dice);
+    }
+
+    */
+    }
+}
+
