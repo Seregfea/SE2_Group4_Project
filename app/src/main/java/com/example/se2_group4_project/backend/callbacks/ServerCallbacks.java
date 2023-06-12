4ef4c18e8fd7415147e4e65e7074cae12a22a1ff
Seregfea <www.Valarauco@gmx.at>
2023-06-12 11:29:53 +0200

    server communication
    made a better server client communication and changed recyclerview to more dynamic reuse

--- app/src/main/java/com/example/se2_group4_project/backend/callbacks/ServerCallbacks.java
+++ app/src/main/java/com/example/se2_group4_project/backend/callbacks/ServerCallbacks.java
@@ -1,9 +1,7 @@
 package com.example.se2_group4_project.backend.callbacks;
 
-import java.net.Socket;
-
 public interface ServerCallbacks {
-    void messageCheat (String message);
+    void messageToALL(String message);
     void onMessageSend(String send);
-    void messageNextPlayer (String message, Integer player);
+    void messageToOne(String message, Integer player);
 }
---
+++ app/src/main/java/com/example/se2_group4_project/backend/callbacks/ServerClientCallbacks.java
@@ -0,0 +1,7 @@
+package com.example.se2_group4_project.backend.callbacks;
+
+import java.io.IOException;
+
+public interface ServerClientCallbacks {
+    void getMessage(String message) throws IOException;
+}
