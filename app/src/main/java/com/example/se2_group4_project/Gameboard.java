4d8fc8abde0a782bfd6956221dc940ba0d9983ff
zhekaandri <36640457+zhekaandri@users.noreply.github.com>
2023-06-13 16:08:53 +0200

    Implement check, if dice is rolled for taking cards

--- app/src/main/java/com/example/se2_group4_project/Gameboard.java
+++ app/src/main/java/com/example/se2_group4_project/Gameboard.java
@@ -453,7 +453,17 @@ public class Gameboard extends AppCompatActivity implements GameboardCallbacks {
                 }
             });
 
-            
+            diceIsRolled = true;
+
+            if (diceIsRolled) {
+                if (!player.isReRoll()) {
+                    activityGameboardBinding.btnRollDice.setEnabled(false);
+                }
+
+                addCardsToPlayer();
+                // call function to flip current card
+                // flipCurrentCardListener();
+            }
 
             testDice();
         });
