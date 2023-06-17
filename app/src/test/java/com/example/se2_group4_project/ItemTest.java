a08cfb5fcf7807bceccd9c88bffddd7da43868ba
zhekaandri <36640457+zhekaandri@users.noreply.github.com>
2023-06-17 15:40:08 +0200

    Add more tests for item cards

--- app/src/test/java/com/example/se2_group4_project/ItemTest.java
+++ app/src/test/java/com/example/se2_group4_project/ItemTest.java
@@ -143,4 +143,91 @@ public class ItemTest {
             itemWasserbombe.isStealable(null);
         });
     }
+
+    @Test
+    public void testPfannenundcoItem() throws JSONException {
+        ArrayList<Integer> badRolledDiceAvailable = new ArrayList<>();
+        badRolledDiceAvailable.add(2);
+        badRolledDiceAvailable.add(3);
+
+        ArrayList<Integer> badRolledDiceSteal = new ArrayList<>();
+        badRolledDiceSteal.add(1);
+        badRolledDiceSteal.add(2);
+        badRolledDiceSteal.add(3);
+
+        ArrayList<Integer> goodRolledDice = new ArrayList<>();
+        goodRolledDice.add(5);
+        goodRolledDice.add(5);
+        goodRolledDice.add(5);
+
+        JSONObject item_Pfannenundco = new JSONObject(
+                "{"+
+                        "number: 5,"+
+                        "count: 2,"+
+                        "stealCard: 3,"+
+                        "itemBenefit: Geschirr dreckig" +
+                        "}"
+        );
+        Item itemPfannenundco = new Item(item_Pfannenundco);
+        assertEquals(5, itemPfannenundco.getNumber());
+        assertEquals(2, itemPfannenundco.getCount());
+        assertEquals(3, itemPfannenundco.getStealCard());
+        assertFalse(itemPfannenundco.isCouchMatschig());
+        assertFalse(itemPfannenundco.isBadewanneDreckig());
+        assertTrue(itemPfannenundco.isGeschirrDreckig());
+
+        assertTrue(itemPfannenundco.isAvailable(goodRolledDice));
+        assertFalse(itemPfannenundco.isAvailable(badRolledDiceAvailable));
+        assertTrue(itemPfannenundco.isStealable(goodRolledDice));
+        assertFalse(itemPfannenundco.isStealable(badRolledDiceSteal));
+        assertThrows(NullPointerException.class, ()->{
+            itemPfannenundco.isAvailable(null);
+        });
+        assertThrows(NullPointerException.class, ()->{
+            itemPfannenundco.isStealable(null);
+        });
+    }
+    @Test
+    public void testKuechenradioItem() throws JSONException {
+        ArrayList<Integer> badRolledDiceAvailable = new ArrayList<>();
+        badRolledDiceAvailable.add(2);
+        badRolledDiceAvailable.add(3);
+
+        ArrayList<Integer> badRolledDiceSteal = new ArrayList<>();
+        badRolledDiceSteal.add(1);
+        badRolledDiceSteal.add(2);
+        badRolledDiceSteal.add(3);
+
+        ArrayList<Integer> goodRolledDice = new ArrayList<>();
+        goodRolledDice.add(2);
+        goodRolledDice.add(2);
+        goodRolledDice.add(2);
+
+        JSONObject item_Kuechenradio = new JSONObject(
+                "{"+
+                        "number: 2,"+
+                        "count: 2,"+
+                        "stealCard: 3,"+
+                        "itemBenefit: Geschirr dreckig" +
+                        "}"
+        );
+        Item itemKuechenradio = new Item(item_Kuechenradio);
+        assertEquals(2, itemKuechenradio.getNumber());
+        assertEquals(2, itemKuechenradio.getCount());
+        assertEquals(3, itemKuechenradio.getStealCard());
+        assertFalse(itemKuechenradio.isCouchMatschig());
+        assertFalse(itemKuechenradio.isBadewanneDreckig());
+        assertTrue(itemKuechenradio.isGeschirrDreckig());
+
+        assertTrue(itemKuechenradio.isAvailable(goodRolledDice));
+        assertFalse(itemKuechenradio.isAvailable(badRolledDiceAvailable));
+        assertTrue(itemKuechenradio.isStealable(goodRolledDice));
+        assertFalse(itemKuechenradio.isStealable(badRolledDiceSteal));
+        assertThrows(NullPointerException.class, ()->{
+            itemKuechenradio.isAvailable(null);
+        });
+        assertThrows(NullPointerException.class, ()->{
+            itemKuechenradio.isStealable(null);
+        });
+    }
 }
